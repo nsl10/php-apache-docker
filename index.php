7a1876a207230942f2e9b4513672242264e81e63
nsl10 <42556882+nsl10@users.noreply.github.com>
2025-03-26 19:37:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 declare(strict_types = 1);
 
 echo '<pre>';
-print_r(getenv('SITE_URL'));
+print_r(getenv('https://php-apache-docker-pw55.onrender.com'));
 echo '<br>';
 print_r($_SERVER);
 echo '</pre>';
